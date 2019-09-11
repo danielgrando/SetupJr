068857fc78919cd95663089ac5e2d2b3a34c2950
Daniel <grando_daniel@hotmail.com>
2019-09-11 16:46:53 -0300

    Mudanças

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+
+//SCROLL
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+function getScrollTopByHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+	window.scroll({
+	top: to,
+    behavior: "smooth",
+     
+    })
+}
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget)- 80;
+	scrollToPosition(to);
+}
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
+});
